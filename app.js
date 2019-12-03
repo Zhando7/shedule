02603d1395e71d398cdad6cae2d86a6ab112de7b
= <=>
2019-12-03 22:59:09 +0600

    .gitignore

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
 module.exports = app;
 
 
