7b65ae30655fe24fc614e364e05029e740d79e0e
Zhandos Erbolovich <zhando797@gmail.com>
2019-12-04 10:15:07 +0600

    Подключение скриптов и стилей

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 3000,
-    // MONGO_URL: 'mongodb://localhost:27017/shedule'
-    MONGO_URL: 'mongodb+srv://zhandos:mongodb123456@newcluster-kv3l3.mongodb.net/test?retryWrites=true&w=majority'
+    MONGO_URL: 'mongodb://localhost:27017/shedule'
+    // MONGO_URL: 'mongodb+srv://zhandos:mongodb123456@newcluster-kv3l3.mongodb.net/test?retryWrites=true&w=majority'
 };
