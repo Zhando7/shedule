e2a97b9f85c4503f7f1d8d0e476c49b6caea56f7
Zhandos Erbolovich <zhando797@gmail.com>
2019-12-20 11:08:57 +0600

    Set's main file the app.js

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const bodyParser = require('body-parser');
 const staticAsset = require('static-asset');
+const conf = require('./config');
 const homeRouter = require('./routes/homeRouter');
 const lessonsRouter = require('./routes/lessonsRouter');
 
+mongoose.Promise = global.Promise;
+mongoose.set('debug', conf.IS_PRODUCTION);
+
+mongoose.connection
+    .on('error', err => console.log(err))
+    .on('close', () => console.log('Database connection closed!'))
+    .on('open', () => {
+        const info = mongoose.connections[0];
+        console.log(`Connected to ${info.host}:${conf.PORT}/${info.name}`);
+    });
+
+mongoose.connect(conf.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// express
 const app = express();
 
 app.set('view engine', 'ejs'); // Для рендеринга страниц задаем движок `ejs`
@@ -19,4 +35,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/lessons', lessonsRouter);
 app.use('/', homeRouter);
 
-module.exports = app;
+app.listen(conf.PORT, () => {
+    console.log(`Web app listening on port:${conf.PORT}`)
+});
--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 3000,
-    // MONGO_URL: 'mongodb://localhost:27017/shedule'
-    MONGO_URL: 'mongodb+srv://zhandos:mongodb123456@newcluster-kv3l3.mongodb.net/test?retryWrites=true&w=majority'
+    IS_PRODUCTION: process.env.NODE_ENV === 'production',
+    MONGO_URL: 'mongodb://localhost:27017/shedule'
+    // MONGO_URL: 'mongodb+srv://zhandos:mongodb123456@newcluster-kv3l3.mongodb.net/test?retryWrites=true&w=majority'
 };
