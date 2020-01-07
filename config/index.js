3f41660fd8b8503ec3c671d5a2e011cffb1f7631
Zhandos Akhmetkarimov <zhando797@gmail.com>
2020-01-07 22:53:26 +0600

    Updated config.js

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
-const dotenv = require('dotenv');
 const path = require('path');
+const dotenv = require('dotenv');
 
 const root = path.join.bind(this, __dirname);
 dotenv.config({ path: root('../.env') });
@@ -8,5 +8,13 @@ module.exports = {
     PORT: process.env.PORT || 3000,
     IS_PRODUCTION: process.env.NODE_ENV,
     MONGO_URL: process.env.MONGO_URL,
-    SESSION_SECRET: process.env.SESSION_SECRET
+    session: {
+        SESSION_SECRET: process.env.SESSION_SECRET,
+        cookie: {
+            originalMaxAge: null,
+            httpOnly: true,
+            path: "/",
+            expires: null
+        }
+    }
 };
