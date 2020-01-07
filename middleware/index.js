606801a2dbbc5991fbf1a2c391920c1d8e6f3241
Zhandos Akhmetkarimov <zhando797@gmail.com>
2020-01-07 22:58:41 +0600

    Updated middleware

--- middleware/index.js
+++ middleware/index.js
@@ -1,12 +1,26 @@
 module.exports = (app, express) => {
-    const path = require('path'),
-        bodyParser = require('body-parser'),
-        staticAsset = require('static-asset'),
-        userRouter = require('../routes/user'),
-        monthRouter = require('../routes/month'),
-        calendarRouter = require('../routes/calendar');
+    const   path = require('path'),
+            conf = require('../config'),
 
+            bodyParser = require('body-parser'),
+            staticAsset = require('static-asset'),
+            
+            mongoose = require('../utils/mongoose'),
+            session = require('express-session'),
+            MongoStore = require('connect-mongo')(session),
+
+            authRouter = require('../routes/authentication'),
+            monthRouter = require('../routes/month'),
+            indexRouter = require('../routes');
+
+    /*
+    * Page rendering
+    */
     app.set('view engine', 'ejs');
+    
+    /*
+    * Requests parser
+    */
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -18,14 +32,25 @@ module.exports = (app, express) => {
     app.use(express.static(path.join(__dirname, '../public')));
 
     /*
-    * Маршрутизация
+    * Session
     */
-    app.use('/login', userRouter);
-    app.use('/month', monthRouter);
-    app.use('/', calendarRouter);
+    app.use(session({
+        secret: conf.session.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+        cookie: conf.session.cookie,
+        store: new MongoStore({ mongooseConnection: mongoose.connection })
+    }));
 
     /*
-    * Обрабатываем ошибки
+    * Routing
+    */
+    app.use('/login', authRouter);
+    app.use('/month', monthRouter);
+    app.use('/', indexRouter);
+
+    /*
+    * Error handlers
     */
     app.use(function(req, res, next) {
         res.status(404);
