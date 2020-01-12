9be1e8cbaa7f5ebcef8117c4e68995b8c594fecc
Zhandos Akhmetkarimov <zhando797@gmail.com>
2020-01-12 20:32:51 +0600

    Remove moment.js

--- config/moment-conf.js
+++
@@ -1,9 +0,0 @@
-const moment = require('moment');
-
-moment.updateLocale('ru', {
-    months: [
-        'января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'
-    ]
-});
-
-module.exports = moment;
