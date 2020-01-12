e7b3a3480d33c0ef1a15be4253056081329198ec
Zhandos Akhmetkarimov <zhando797@gmail.com>
2020-01-12 20:24:34 +0600

    Updated models

--- models/shedule/index.js
+++ models/shedule/index.js
@@ -2,14 +2,22 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // parse json
-const month = require('./monthSchema.json');
+const   month = require('./monthSchema.json'),
+        day = require('./daySchema.json'),
+        lesson = require('./lessonSchema.json');
 
 // create a schema
-const monthSchema = Schema(month, { versionKey: false });
+const   monthSchema = Schema(month, { versionKey: false }),
+        daySchema = Schema(day, { versionKey: false }),
+        lessonSchema = Schema(lesson, { versionKey: false });
 
 // compile a schema
-const Month = mongoose.model('Months', monthSchema);
+const   Month = mongoose.model('Months', monthSchema),
+        Day = mongoose.model('Days', daySchema),
+        Lesson = mongoose.model('Lessons', lessonSchema);
 
 module.exports = [{
-    Month: Month
+    Month: Month,
+    Day: Day,
+    Lesson: Lesson
 }];
