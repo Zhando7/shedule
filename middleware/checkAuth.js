77f1bd2a5d1f3a6e538aa146b59f8a523c3a6887
Zhandos Erbolovich <zhando797@gmail.com>
2020-01-13 16:35:32 +0600

    Updated checkAuth.js

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -1,8 +1,7 @@
 module.exports = (req, res, next) => {
     if(!req.session.userId) {
-        res.send('You are not admin!')
+        res.send('You are not admin!');
     } else {
-        console.log('You are admin!');
         next();
     }
 }
