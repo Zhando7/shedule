8eaf78319c89dd2212d0f214165af5443e26da03
Zhandos Erbolovich <zhando797@gmail.com>
2020-01-16 18:19:28 +0600

    Updated

--- controllers/admin/month/index.js
+++ controllers/admin/month/index.js
@@ -53,7 +53,37 @@ exports.getMonth = (req, res) => {
 }
 
 exports.updateMonth = (req, res) => {
-    
+    if(!req.body) return res.status(400).json({
+        msg: 'The request body is null'
+    });;
+
+    const id = req.body.id;
+    const number = req.body.number;
+    const title = req.body.title;
+    const year = req.body.year;
+
+    Shedule.Month.updateOne({ id: id }, (err, doc) => {
+        if(err) {
+            return res.status(400).json({
+                msg: 'Data do not update'
+            });
+        }
+        console.log(doc);
+        doc.month.number = number;
+        doc.month.title = title;
+        doc.year = year;
+        doc.save((err) => {
+            if(err) {
+                return res.status(400).json({
+                    msg: 'Document do not save'
+                });
+            }
+            res.status(200).json({
+                msg: 'Data is updated',
+                doc: doc
+            })
+        });
+    });
 }
 
 exports.deleteMonth = (req, res) => {
