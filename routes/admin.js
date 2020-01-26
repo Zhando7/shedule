5e551da89708aa40b0b6c3a9129530cca3cf758d
Zhandos Akhmetkarimov <zhando797@gmail.com>
2020-01-26 22:10:55 +0600

    Updated admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -3,26 +3,31 @@ const adminRouter = express.Router();
 const adminController = require('../controllers/admin');
 
 // Create month, day, lesson
+adminRouter.post('/year', adminController.year.createYear);
 adminRouter.post('/month', adminController.month.createMonth);
 adminRouter.post('/day', adminController.day.createDay);
 adminRouter.post('/lessons', adminController.lesson.createLesson);
 
 // Read index, month, day, lessons
 adminRouter.get('/', adminController.getIndex);
+adminRouter.get('/year/:id', adminController.year.getYear);
 adminRouter.get('/month/:id', adminController.month.getMonth);
 adminRouter.get('/day/:id', adminController.day.getDay);
 
 // Update month, day, lesson
+adminRouter.put('/year', adminController.year.updateYear);
 adminRouter.put('/month', adminController.month.updateMonth);
 adminRouter.put('/day', adminController.day.updateDay);
 adminRouter.put('/lessons', adminController.lesson.updateLesson);
 
 // Delete month, day, lesson
+adminRouter.delete('/year/:id', adminController.year.deleteYear);
 adminRouter.delete('/month/:id', adminController.month.deleteMonth);
 adminRouter.delete('/day/:id', adminController.day.deleteDay);
 adminRouter.delete('/lessons/:id', adminController.lesson.deleteLesson);
 
 // Select month, day, lesson
+adminRouter.get('/year/select/:id', adminController.year.selectYear);
 adminRouter.get('/month/select/:id', adminController.month.selectMonth);
 adminRouter.get('/day/select/:id', adminController.day.selectDay);
 adminRouter.get('/lessons/select/:id', adminController.lesson.selectLesson);
