394178fbf5bcbb54c0827afe84c6e56ff5c70208
Zhandos Erbolovich <zhando797@gmail.com>
2020-01-30 17:26:34 +0600

    updated admin/index.js

--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -1,14 +1,21 @@
 const   year = require('./year'),
         month = require('./month'),
         day = require('./day'),
-        lesson = require('./lesson');
+        lesson = require('./lesson'),
+        Shedule = require('../../models/shedule'),
+        admin = require('../../utils/admin');
 
 module.exports = {
     year: year,
     month: month,
     day: day,
     lesson: lesson,
-    getIndex: (req, res) => {
-        res.render('admin');
+    getIndex: async (req, res) => {
+        try {
+            const docs = await Shedule.Year.find({});
+            res.render('admin', { docs });
+        } catch (err) {
+            admin.sendError(err, res, 'Years not found');
+        }
     }
 }
