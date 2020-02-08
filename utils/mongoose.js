183473353767c5cf165cbfae09c3dcd0435a9dc6
Zhandos Akhmetkarimov <zhando797@gmail.com>
2020-02-08 10:23:03 +0600

    updated of the promise message

--- utils/mongoose.js
+++ utils/mongoose.js
@@ -9,7 +9,7 @@ mongoose.set('debug', ( conf.IS_PRODUCTION === 'prod' ) ? false : true );
 mongoose.connect(conf.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 mongoose.connection
     .on('error', err => console.log(err))
-    .on('close', () => console.log('Database connection closed!'))
+    .on('close', () => console.log('Database connection is closed!'))
     .on('open', () => {
         const info = mongoose.connections[0];
         console.log(`Mongoose is connected to ${info.host}:${conf.PORT}/${info.name}`);
