98dcb497aa8b60578a3f2977a074091d52c016a8
Zhandos Akhmetkarimov <zhando797@gmail.com>
2020-02-08 16:20:38 +0600

    refactoring

--- models/shedule/index.js
+++ models/shedule/index.js
@@ -4,24 +4,24 @@ const Schema = mongoose.Schema;
 // parse json
 const   year = require('./yearSchema.json'),
         month = require('./monthSchema.json'),
-        day = require('./daySchema.json'),
+        date = require('./dateSchema.json'),
         lesson = require('./lessonSchema.json');
 
 // create a schema
 const   yearSchema = Schema(year, { versionKey: false }),
         monthSchema = Schema(month, { versionKey: false }),
-        daySchema = Schema(day, { versionKey: false }),
+        dateSchema = Schema(date, { versionKey: false }),
         lessonSchema = Schema(lesson, { versionKey: false });
 
 // compile a schema
 const   Year = mongoose.model('Years', yearSchema),
         Month = mongoose.model('Months', monthSchema),
-        Day = mongoose.model('Days', daySchema),
+        nDate = mongoose.model('Dates', dateSchema),
         Lesson = mongoose.model('Lessons', lessonSchema);
 
 module.exports = {
     Year: Year,
     Month: Month,
-    Day: Day,
+    nDate: nDate,
     Lesson: Lesson
 };
