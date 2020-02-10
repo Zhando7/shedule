37790accacc7972044cde0d1515896dc983a902f
Zhandos Erbolovich <zhando797@gmail.com>
2020-02-10 11:44:19 +0600

    modified utils/mongoose.js

--- utils/mongoose.js
+++ utils/mongoose.js
@@ -3,16 +3,17 @@ conf = require('../config'),
 /*
  * Database connection
  */
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
+mongoose.createConnection( conf.MONGO_URL );
 mongoose.set('debug', ( conf.IS_PRODUCTION == 'prod' ) ? false : true );
 
-mongoose.connect(conf.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
-mongoose.connection
-    .on('error', err => console.log(err))
-    .on('close', () => console.log('Database connection is closed!'))
-    .on('open', () => {
-        const info = mongoose.connections[0];
-        console.log(`Mongoose is connected to ${info.host}:${conf.PORT}/${info.name}`);
-    });
+// mongoose.connect(conf.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+// mongoose.connection
+//     .on('error', err => console.log(err))
+//     .on('close', () => console.log('Database connection is closed!'))
+//     .on('open', () => {
+//         const info = mongoose.connections[0];
+//         console.log(`Mongoose is connected to ${info.host}:${conf.PORT}/${info.name}`);
+//     });
 
 module.exports = mongoose;
