446d1996fa94f5e1b762c646afabb0399c9cc254
Zhandos Erbolovich <zhando797@gmail.com>
2020-02-11 10:10:49 +0600

    updated

--- controllers/admin/date/index.js
+++ controllers/admin/date/index.js
@@ -9,6 +9,7 @@ exports.createDate = async (req, res) => {
                 createNewDate = new Shedule.nDate({ id_month, full_date }),
                 docs = await createNewDate.save();
         console.log(docs);
+        console.log(req.body);
         admin.sendResult(res, docs);
     } catch (err) {
         admin.sendError(err, res);
