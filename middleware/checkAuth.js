1a1d29f88c2424aaccd2badcf6bcccf7856acc78
Akhmetkarimov Zhandos <zhando797@gmail.com>
2020-07-01 07:47:14 +0600

    more exictly check

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -1,9 +1,9 @@
 module.exports = (req, res, next) => {
-    if(!req.session.userId) {
+    if(!!req.session.userId) {
+        next();
+    } else {
         res.status(403).json({
             msg: "You are not admin!"
         });
-    } else {
-        next();
     }
 }
